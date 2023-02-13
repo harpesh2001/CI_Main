25e4fa5fd0153feb118c988824f39885f1c6818c
Harpesh Patel <110534999+harpesh2001@users.noreply.github.com>
2023-02-13 15:52:00 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return PartialView();
+        }
+        
+        public IActionResult ForgotPassword()
+        {
+            return PartialView();
+        }
+        public IActionResult ResetPassword()
+        {
+            return PartialView();
+        }
+        public IActionResult Register()
+        {
+            return PartialView();
+        }
+    }
+}
