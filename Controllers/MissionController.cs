ca08821dd829408d94f246ad93f16db2eb94ba94
Harpesh Patel <110534999+harpesh2001@users.noreply.github.com>
2023-02-21 18:18:33 +0530

    mission detailPage

---
+++ Controllers/MissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult MIssionDetail()
+        {
+            return View();
+        }
+    }
+}
